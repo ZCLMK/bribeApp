168256519dbb6b60b88834fd91ff6d25644a0c58
EC2 Default User <ec2-user@ip-172-31-18-170.eu-west-1.compute.internal>
2018-06-27 08:08:13 +0000

    with nice ass grid

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -2,4 +2,12 @@ module EventHelper
   def some_events_from_category(limit, id)
 	return Event.where(category_id: id)
   end 
+  
+  def display_default_image(param)
+	  	if param.image?
+	  	return "param.image.thumb.url"
+	  else 
+	  	return "https://picsum.photos/200/300/?random"
+	  end 
+  end
 end
